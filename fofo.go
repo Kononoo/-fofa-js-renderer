a522c1a2176d3dceeb22be83037131685a9b798b
liangjianjun <liangjianjun@baimahui.net>
2024-07-11 19:28:07 +0800

    feat:FOFA-通过接口获取目标

---
+++ fofo.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type FofaResponse struct {
+	Results [][]string `json:"results"`
+}
+
+func getFofaResults(apiKey, query string) ([]string, error) {
+	encodedQuery := base64.StdEncoding.EncodeToString([]byte(query))
+	url := fmt.Sprintf("https://fofa.info/api/v1/search/all?key=%s&qbase64=%s&fields=host&size=3000", apiKey, encodedQuery)
+
+	req, err := http.NewRequest("Get", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get fofa results -> status code: %d", resp.StatusCode)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var fofaResponse FofaResponse
+	if err = json.Unmarshal(body, &fofaResponse); err != nil {
+		return nil, err
+	}
+
+	var urls []string
+	for _, result := range fofaResponse.Results {
+		urls = append(urls, result[0])
+	}
+
+	return urls, nil
+}
